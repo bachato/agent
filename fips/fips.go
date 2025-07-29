225af93cea0ca6c3a5e9f358a715f5ae061c22aa
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2025-07-29 18:35:12 -0300

    fix(fips): simplify the initialization logic BE-12108 (#956)

--- fips/fips.go
+++ fips/fips.go
@@ -1,44 +1,32 @@
 package fips
 
 import (
-	"os"
-	"strings"
+	"crypto/fips140"
+	"sync"
 
 	"github.com/rs/zerolog/log"
 )
 
-const goDebugEnvVar = "GODEBUG"
-const fipsGoDebugVar = "fips140"
+var fipsMode, isInitialised bool
 
-var fipsMode = false
-var isInitialised = false
+var once sync.Once
 
 func InitFIPS(enabled bool) {
-	isInitialised = true
-	fipsMode = enabled
-	if enabled {
-		godebugFlags := os.Getenv(goDebugEnvVar)
-		pairs := strings.Split(godebugFlags, ",")
-		// TODO: once we update to go 1.24 then we can use https://pkg.go.dev/crypto/fips140#Enabled to check this.
-		fipsGodebugVar := false
-		for _, pair := range pairs {
-			k, v, found := strings.Cut(pair, "=")
-			if found && k == fipsGoDebugVar && (v == "on" || v == "only") {
-				fipsGodebugVar = true
-				break
-			}
-		}
+	once.Do(func() {
+		isInitialised = true
+		fipsMode = enabled
 
-		if enabled && !fipsGodebugVar {
+		if enabled && !fips140.Enabled() {
 			log.Fatal().Msg("If FIPS mode is enabled then the fips140 GODEBUG environment variable must be set")
 		}
-	}
+	})
 }
 
 func FIPSMode() bool {
 	if !isInitialised {
 		log.Fatal().Msg("Could not determine if FIPS mode is enabled because InitFIPS was never called")
 	}
+
 	return fipsMode
 }
 
@@ -46,5 +34,6 @@ func CanTLSSkipVerify() bool {
 	if !isInitialised {
 		log.Fatal().Msg("Could not determine if FIPS mode is enabled because InitFIPS was never called")
 	}
+
 	return !fipsMode
 }
