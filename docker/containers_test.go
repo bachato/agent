929a48f0418b202b8846b99c0aaffd8f369bcf27
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2025-08-13 19:42:24 -0300

    fix(linters): add many linters BE-12112 (#1009)

--- docker/containers_test.go
+++ docker/containers_test.go
@@ -1,15 +1,13 @@
 package docker
 
 import (
-	"os"
 	"testing"
 
 	"github.com/docker/docker/api/types/container"
-	"github.com/stretchr/testify/require"
 )
 
 func TestContainerWait(t *testing.T) {
-	require.NoError(t, os.Setenv("DOCKER_HOST", "invalid-host"))
+	t.Setenv("DOCKER_HOST", "invalid-host")
 
 	statusCh, errCh := ContainerWait("container-name", container.WaitConditionNotRunning)
 	select {
